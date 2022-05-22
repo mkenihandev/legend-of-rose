d115cc83a3833290bcdbac3fb085c48344bfee73
mkenihandev <mkenihan96@gmail.com>
2022-05-22 16:15:57 +0000

    Add proper inventory functionality for scene one

--- run.py
+++ run.py
@@ -2,8 +2,20 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-inventory = []
-room_one = ['torch']
+inventory = ['ladybug', 'test vial', 'garbage bag']
+
+class Human(object):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+
+bandit = Human('Bandit', 100)
+
+
+print('You attack the bandit!')
+bandit.health -= 50
+print(f'The Bandits health is now {bandit.health}')
 
 
 def good_or_bad():
@@ -19,57 +31,66 @@ def good_or_bad():
 
 
 def scene_one():
-    print(room_one)
     print("""
     What do you do?
-    (Look around/Open Door/Inventory)
+    (E.g. Look around/Open Door/Inventory)
     """)
     answer = ''
-    while answer != 'look around' and answer != 'open door' and answer != 'inventory':
+    while (answer == ''):
         answer = input('').lower()
-        if answer == 'look around':
-            print('You see an unlit torch lying on the ground. Take it?')
-            torch_take = input('').lower()
-            if torch_take == 'yes' and 'torch' in room_one:
-                inventory.append('torch')
-                scene_one()
-                room_one.remove('torch')
-            elif torch_take == 'yes' and 'torch' not in room_one:
-                print('You already have that')
-            elif torch_take == 'no':
-                scene_one()
+        if answer == 'look around' and 'torch' not in inventory:
+            print('You see an unlit torch lying on the ground.')
+            scene_one()
+        elif answer == 'look around' and 'torch' in inventory:
+            print('You see the soot from where the torch used to be.')
+            scene_one()
         elif answer == 'inventory':
-            print(inventory)
+            print('The items in your inventory:')
+            for item in inventory:
+                print(f'- {item}')
             scene_one()
         elif answer == 'open door':
-            print('onward')
+            print('Next Scene')
+            break
         elif answer == 'stay here':
             print('You have died. Restart.')
+            break
+        elif 'torch' in answer and 'torch' not in inventory:
+            print('You pick up the torch.')
+            inventory.append('torch')
+            scene_one()
+        elif answer == 'quit':
+            print('The Rose awaits your next attempt.')
+            break
+        else:
+            print('You cannot do that.')
+            scene_one()
 
 
-print("""
-Welcome Hero to The Legend of Rose.
-Your goal is to obtain the legendary Rose held in Castle Rosebush.
-Revered for it's incredible ability to flatter the one you love, 
-it is well guarded.
-You will face many challenges along the way, and the choices you make may 
-affect the outcome.\n""")
 
-hero_name = input('Please enter your characters name: ')
+# print("""
+# Welcome Hero to The Legend of Rose.
+# Your goal is to obtain the legendary Rose held in Castle Rosebush.
+# Revered for it's incredible ability to flatter the one you love, 
+# it is well guarded.
+# You will face many challenges along the way, and the choices you make may 
+# affect the outcome.\n""")
 
-print(f'\nAh, {hero_name}, a fine name for a valiant Knight.\n')
+# hero_name = input('Please enter your characters name: ')
 
-alignment = good_or_bad()
+# print(f'\nAh, {hero_name}, a fine name for a valiant Knight.\n')
 
-print(f'\nYou are {alignment}, interesting.')
+# alignment = good_or_bad()
 
-print("""
-    You stand in a single square room of a sprawling dungeon. 
-    You were told at the end of this dungeon, a secret passage can be found 
-    leading directly to the throne room of Caslte Rosebush, 
-    where the Rose is held.
-    Ahead of you, there is a door.
-    """)
+# print(f'\nYou are {alignment}, interesting.')
 
-scene_one()
-print(room_one)
+# print("""
+#     You stand in a single square room of a sprawling dungeon. 
+#     You were told at the end of this dungeon, 
+#     a secret passage can be found 
+#     leading directly to the throne room of Caslte Rosebush, 
+#     where the Rose is held.
+#     Ahead of you, there is a door.
+#     """)
+
+# scene_one()
